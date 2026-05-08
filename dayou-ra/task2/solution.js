690a97c2572a277efcd418cf5d0652daa97595b5
RA Dayou <bora.dayou@gmail.com>
2026-05-08 23:47:23 +0700

    Finish the second task

---
+++ dayou-ra/task2/solution.js
@@ -0,0 +1,30 @@
+
+const url = 'https://api.example.com/users'
+async function fetchWithRetry(url, maxRetries = 3){
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+    for (let i = 0; i < maxRetries; i++){
+        try {
+
+            const response = await fetch(url);
+
+            if (!response.ok){
+                throw new Error(`error to fetch ${url}`);
+            };
+
+            const data = await response.json();
+            console.log(data);
+            return data
+        } catch(error) {
+            console.error(`Attempt ${i+1} failed`);    
+            if (i < maxRetries - 1) {
+                const backoffDelay = 100 * Math.pow(2, i);
+                await delay(backoffDelay);
+            };
+        };
+    };
+    throw new Error(`Failed to fetch '${url}' after ${maxRetries} attempts`)
+};  
+await fetchWithRetry(url)
+
+module.exports = fetchWithRetry;
